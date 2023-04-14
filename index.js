b08d054f7477095a37286b1581dbf8a214b7f52a
Keara Harris <keara.s.harris@gmail.com>
2023-04-14 16:28:54 -0500

    Completed first class functions lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+//Declare varaibale with const and assign an anonymous function that returns a new array of the first two drivers
+const returnFirstTwoDrivers = ((arrDrivers) => {
+    return arrDrivers.slice(0,2)
+    
+
+
+})
+//Declare variable with const and assign an anoymous function that returns new array of last two drivers
+
+const returnLastTwoDrivers = ((arrDrivers) => {
+   return arrDrivers.slice(2, arrDrivers.length)
+  
+    
+
+
+})
+//Create an array containing two elements from retrunFirstTwoDrivers and returnLastTwoDrivers functions.
+
+const selectingDrivers =  [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integerValue){
+    return function (fare) { 
+        return fare * integerValue
+        }
+}
+
+const fareDoubler = (fare) => {
+    return createFareMultiplier(2)(fare)
+}
+
+const fareTripler = function(fare){
+    return createFareMultiplier(3)(fare)
+}
+const selectDifferentDrivers = (arrDrivers, returnFirstOrLast) =>{
+
+   return returnFirstOrLast(arrDrivers);
+   
+}
